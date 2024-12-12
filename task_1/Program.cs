6c4118cf4a8e5e0480db74262ab772e9caba8040
RadwaRoute <radwaa.route@gmail.com>
2024-12-12 22:21:41 +0200

    add assignment

---
+++ task_1/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 
+            //  1)
+            //Console.WriteLine("enter a number " );
+            //int x = int.Parse( Console.ReadLine());
+            //Console.WriteLine(x);
+
+
+            // 2)
+            //String x = "menna";
+            //int x2 = Convert.ToInt32(x);
+            //Console.WriteLine(x2);
+
+            // can't handle this bec x contain non numric value and it can't convert this or handle this error
+            // but if it hold numeric value that will run
+
+            //  3)
+            //Console.WriteLine("enter first number ");
+            //float x = float.Parse(Console.ReadLine());
+            //Console.WriteLine("enter second number ");
+            //float y = float.Parse(Console.ReadLine());
+            //float sum = x + y;
+            //Console.WriteLine("sum is " + sum);
+
+
+            //  4)
+            //string str = "Helloo I'm Menna Maher";
+            //string newString = str.Substring(7, 12);
+            //Console.WriteLine(newString);
+
+
+            //  5)
+
+            //int x = 5;
+            //int y = 4;
+            //Console.WriteLine("x= "+x); // x=5
+            //Console.WriteLine("y= "+y); // y=4
+            //y = x;
+            //Console.WriteLine("y= "+y); //y=5
+            //x = 3;
+            //Console.WriteLine("x= "+x); // x=3
+            //Console.WriteLine("y= "+y); // y still 5 not change
+
+
+            //  6)
+
+            //point p1;
+            //point p2;
+            //p1 = new point();
+            //p2 = new point();
+            //p1.x = 4;
+            //p2.x = 6;
+            //Console.WriteLine(p1.x);
+            //Console.WriteLine(p2.x);
+            //p1 = p2;
+            //p1.x = 7; 
+            //Console.WriteLine(p1.x); //p1=7
+            //Console.WriteLine(p2.x);  // p2=7
+            //// once change value of one the other change
+
+
+            //  7)
+
+            //string str1= Console.ReadLine();
+            //string str2= Console.ReadLine();
+            //Console.WriteLine(str1 + str2);
+
+
+            //  8)
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            //Console.WriteLine(d);
+            //// anser is (b)
+
+
+
+            //  9)
+
+            //Console.WriteLine(13 / 2 + " " + 13 % 2);
+
+            // answer is (d)
+
+
+            //  10)
+
+            //int num = 1, z = 5;
+
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+
+            // answer is (d)
+
+
+
+
+
+
+            #endregion
+        }
+    }
+}
